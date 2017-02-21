a54460046c412b49f6eeb9b9a510cda2b36455a3
metcalfepete <metcalfepete@hotmail.com>
2017-02-21 12:59:33 -0500

    Add files via upload

---
+++ 4buttons.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>PHP/Pi Rover Controls</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+
+  <h2>PI Four Button Example</h2>
+  <form action="" method="post">
+    <div class="form-group">
+
+    <button type="submit" name="submit" class="btn-success btn-lg" style="width:100%" value="go">Forward</button>
+    <button type="submit" name="submit" class="btn-info btn-lg" style="width:49%" value="left">Left</button>
+    <button type="submit" name="submit" class="btn-info btn-lg" style="width:49%" value="right">Right</button>
+    <button type="submit" name="submit" class="btn-danger btn-lg" style="width:100%" value="stop">Stop</button>
+  </form>
+</div>
+
+</body>
+</html>
+
